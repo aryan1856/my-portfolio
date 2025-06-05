1467e2b385fcde7c24cc15371d014a7b34622c90
Aryan-Diwakar-1856 <diwakerryan7900@gmail.com>
2025-06-05 19:04:57 +0530

    Education Component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import EducationCard from './EducationCard'
+import { FaUserGraduate } from "react-icons/fa";
 
 const Education = () => {
+    const undergrad_image = "/images/Dbgi_logo.png";
+    const postgrad_image = "/images/mnnit.png";
+    const school_ugrad = "Dev Bhoomi Group of Institutions, Dehradun";
+    const school_pgrad = "Motilal Nehru National Institute of Technology Allahabad, Prayagraj";
+    const degree_ugrad = "Bachelors in Computer Applications";
+    const degree_pgrad = "Masters in Computer Applications";
+    const duration_ugrad = "Sep 2020 - Aug 2023";
+    const duration_pgrad = "Aug 2023 - present";
   return (
-    <div>Education</div>
+    <div className="flex justify-center items-center flex-col p-4 w-full">
+
+    <div className="flex items-center justify-center text-3xl sm:text-4xl md:text-7xl font-[Dancing_Script] tracking-wide font-bold gap-4 p-4">
+        <h1>Education</h1>
+        <FaUserGraduate className='animate-pulse'/>
+    </div>
+
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-[90%] mt-8">
+        <EducationCard image={undergrad_image} school={school_ugrad} degree={degree_ugrad} duration={duration_ugrad}/>
+        <EducationCard image={postgrad_image} school={school_pgrad} degree={degree_pgrad} duration={duration_pgrad}/>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/EducationCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useInView } from 'react-intersection-observer'
+
+const EducationCard = ({ image, school, degree, duration }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.01, // lower for mobile
+  })
+
+  return (
+    <div
+      ref={ref}
+      className={`flex justify-center items-center flex-col p-2 m-4 text-center will-change-transform transition-all duration-1000 ease-out 
+      ${inView ? 'opacity-100 scale-100' : 'opacity-0 scale-75'} hover:shadow-xl/30`}
+    >
+      <div>
+        <img className='max-w-62 max-h-43' src={image} alt="school" />
+      </div>
+      <div className='font-bold text-3xl p-4'>
+        <h2>{school}</h2>
+      </div>
+      <div className='text-xl p-2'>
+        <h4>{degree}</h4>
+      </div>
+      <div>
+        <p>{duration}</p>
+      </div>
+    </div>
+  )
+}
+
+export default EducationCard
